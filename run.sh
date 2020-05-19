11edd09b50fa0c3cc73a15edfa0a79b54bc24519
artemkulish123 <artemkulish123@gmail.com>
2020-05-19 16:08:59 +0300

    Changed build type to war

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-./SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=trip
-
-java -jar *.jar
+bash /opt/tomcat/bin/catalina.sh start
+cd /home && ./SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=trip &
+tail -f /dev/null
--- src/main/java/com/softserve/kickscootertrip/KickScooterTripApplication.java
+++ src/main/java/com/softserve/kickscootertrip/KickScooterTripApplication.java
@@ -3,11 +3,17 @@ package com.softserve.kickscootertrip;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @EnableFeignClients
-public class KickScooterTripApplication {
+public class KickScooterTripApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KickScooterTripApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(KickScooterTripApplication.class, args);
     }
